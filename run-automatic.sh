3c5f25c03761651eafa94b6d524a6ae507502192
Matheus <teusanastacio@gmail.com>
2021-11-28 19:32:31 -0300

    sh automatic

---
+++ run-automatic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+while true
+do
+  echo "tudo de novo"
+  sleep 2
+  rm -rf ./output/*
+  python crawler/google-news-crawler.py
+  python crawler/coinmarketcap-crawler.py
+  git add ./output/*
+  git commit -m"update data $(date +%d/%m/%Y" - "%H:%M:%S)"
+  git push origin main
+
+  echo "durmi um poquinho"
+  sleep 5
+done
